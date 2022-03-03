be541faed28c583e07872f6b2f17569bf1ed25f8
Kevin <kevink3126@gmail.com>
2022-03-03 13:22:56 +0800

    test_9适用于Linkvertise

--- main.py
+++ main.py
@@ -34,62 +34,62 @@ if __name__ == '__main__':
             }
             browser = webdriver.Chrome(seleniumwire_options=options)
             time.sleep(3)
-            site_url = 'https://tei.ai/kPPKII'
+            site_url = 'https://direct-link.net/416534/breaking-bad-extras'
             browser.get(site_url)
-            # 如果出现人机验证，破解
-            try:
-                recaptch_obj = browser.find_element(By.CLASS_NAME, 'g-recaptcha')
-                google_sitekey = recaptch_obj.get_attribute('data-sitekey')
-                print(google_sitekey)
-                print('Beginning solve reCaptcha ... ')
-                result = solver.recaptcha(sitekey=google_sitekey, url=site_url)
-                google_captcha_response_input = browser.find_element(
-                    By.ID, 'g-recaptcha-response')
-
-                # make input visible
-                browser.execute_script(
-                    "arguments[0].setAttribute('style','type: text; visibility:visible;');",
-                    google_captcha_response_input)
-                # input the code received from 2captcha API
-                google_captcha_response_input.send_keys(result.get('code'))
-                # hide the captch input
-                browser.execute_script(
-                    "arguments[0].setAttribute('style', 'display:none;');",
-                    google_captcha_response_input)
-                balance = solver.balance()
-                # show 2captcha Balance.
-                print(f'Your 2captcha balance is ${round(balance, 2)}')
-                print('Solved.')
-            except:
-                print('Can not resovle Google reCaptcha object. Error.')
-
-            # 处理人机验证后的逻辑
-            submit_btn = browser.find_element(By.ID, 'continue')
-            browser.execute_script(
-                "arguments[0].removeAttribute('disabled');",
-                submit_btn)
-            # 点击页面会有3个弹出页面，要等待3个弹出页面后，才能进行页面跳转
-            page_title = browser.title
-            while page_title == 'Loan2Host':
-                time.sleep(1)
-                submit_btn.click()
-                windows_handles = browser.window_handles
-                browser.switch_to.window(windows_handles[0])
-                page_title = browser.title
-
-            try:
-                browser.find_element(By.CSS_SELECTOR, '.fc-button.fc-cta-consent.fc-primary-button').click()
-            except:
-                print('There is no popup window that you need to click, continue.')
-
-            time.sleep(15)
-            browser.find_element(By.XPATH, "//a[@href='#getlink']").click()
-            browser.find_element(By.ID, 'getlink').click()
-            time.sleep(15)
-            element = browser.find_element(By.XPATH, "//a[@class='btn btn-success btn-lg get-link']")
-            browser.execute_script('arguments[0].click();', element)
-            # browser.find_element(By.XPATH, "//a[@class='btn btn-success btn-lg get-link']").click()
-            print('Done.')
+            # # 如果出现人机验证，破解
+            # try:
+            #     recaptch_obj = browser.find_element(By.CLASS_NAME, 'g-recaptcha')
+            #     google_sitekey = recaptch_obj.get_attribute('data-sitekey')
+            #     print(google_sitekey)
+            #     print('Beginning solve reCaptcha ... ')
+            #     result = solver.recaptcha(sitekey=google_sitekey, url=site_url)
+            #     google_captcha_response_input = browser.find_element(
+            #         By.ID, 'g-recaptcha-response')
+            #
+            #     # make input visible
+            #     browser.execute_script(
+            #         "arguments[0].setAttribute('style','type: text; visibility:visible;');",
+            #         google_captcha_response_input)
+            #     # input the code received from 2captcha API
+            #     google_captcha_response_input.send_keys(result.get('code'))
+            #     # hide the captch input
+            #     browser.execute_script(
+            #         "arguments[0].setAttribute('style', 'display:none;');",
+            #         google_captcha_response_input)
+            #     balance = solver.balance()
+            #     # show 2captcha Balance.
+            #     print(f'Your 2captcha balance is ${round(balance, 2)}')
+            #     print('Solved.')
+            # except:
+            #     print('Can not resovle Google reCaptcha object. Error.')
+            #
+            # # 处理人机验证后的逻辑
+            # submit_btn = browser.find_element(By.ID, 'continue')
+            # browser.execute_script(
+            #     "arguments[0].removeAttribute('disabled');",
+            #     submit_btn)
+            # # 点击页面会有3个弹出页面，要等待3个弹出页面后，才能进行页面跳转
+            # page_title = browser.title
+            # while page_title == 'Loan2Host':
+            #     time.sleep(1)
+            #     submit_btn.click()
+            #     windows_handles = browser.window_handles
+            #     browser.switch_to.window(windows_handles[0])
+            #     page_title = browser.title
+            #
+            # try:
+            #     browser.find_element(By.CSS_SELECTOR, '.fc-button.fc-cta-consent.fc-primary-button').click()
+            # except:
+            #     print('There is no popup window that you need to click, continue.')
+            #
+            # time.sleep(15)
+            # browser.find_element(By.XPATH, "//a[@href='#getlink']").click()
+            # browser.find_element(By.ID, 'getlink').click()
+            # time.sleep(15)
+            # element = browser.find_element(By.XPATH, "//a[@class='btn btn-success btn-lg get-link']")
+            # browser.execute_script('arguments[0].click();', element)
+            # # browser.find_element(By.XPATH, "//a[@class='btn btn-success btn-lg get-link']").click()
+            # print('Done.')
             time.sleep(10)
             browser.quit()
         except:
--- test_9.py
+++ test_9.py
@@ -6,50 +6,61 @@ import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import random
 
 new_binary_path = '/Applications/Tor Browser.app/Contents/MacOS/firefox'
 ops = options()
 ops.binary_location = new_binary_path
 
-# firefox_binary = FirefoxBinary(binary)
+url_list = [
+    'https://link-target.net/416534/costa-rica-in-4k',
+    'https://direct-link.net/416534/breaking-bad-extras',
+    'https://link-center.net/416534/make-chocolate-cake',
+    'https://link-target.net/416534/50-facts-in-breaking-bad'
+]
 
 if __name__ == "__main__":
     var = 1
     while var == 1:
         try:
             print('========================')
-            print('启动Tor浏览器，并获取IP地址')
+            print('启动Tor浏览器')
             browser = webdriver.Firefox(options=ops)
             browser.find_element(By.ID, 'connectButton').click()
-            time.sleep(10)
-            browser.get('http://ipinfo.io/json')
-            html = browser.page_source
-            soup = BeautifulSoup(html, 'lxml')
-            dict_from_json = json.loads(soup.text)
-            print('启动成功，网络环境如下：')
-            print('当前IP：' + dict_from_json['ip'])
-            print('当前城市：' + dict_from_json['city'])
-            print('当前地区：' + dict_from_json['region'])
-            print('当前国家：' + dict_from_json['country'])
-            time.sleep(1)
-            site_url = 'https://link-center.net/416534/make-chocolate-cake'
-            browser.get(site_url)
-            time.sleep(3)
+            browser.implicitly_wait(10)
+            while var == 1:
+                if browser.current_url == 'about:blank':
+                    site_url = random.choice(url_list)
+                    # site_url = 'https://direct-link.net/416534/breaking-bad-extras'
+                    browser.set_page_load_timeout(60)
+                    try:
+                        browser.get(site_url)
+                    except Exception as timeout:
+                        browser.quit()
+                    break
         except:
             continue
         #跳过人机验证，直接访问URL
         try:
-            browser.find_element(By.XPATH, '//button[@class=" css-6dzqka"]').click()
+            element = WebDriverWait(browser, 20, 0.5).until(
+                EC.visibility_of_element_located((By.XPATH, '//div[@id="qc-cmp2-ui"]//button[@class=" css-6dzqka"]'))
+            )
+            element.click()
         except:
             None
 
         try:
-            browser.execute_script("___grecaptcha_cfg.clients['0'].l.l.callback()")
-            #有时候用户协议会出现的比较晚，在真正处理逻辑之前尝试点击一次用户协议，点击正确与否都继续下面的逻辑处理
             try:
-                browser.find_element(By.XPATH, '//button[@class=" css-6dzqka"]').click()
+                browser.execute_script("___grecaptcha_cfg.clients['0'].l.l.callback()")
             except:
                 None
+            #有时候用户协议会出现的比较晚，在真正处理逻辑之前尝试点击一次用户协议，点击正确与否都继续下面的逻辑处理
+            # try:
+            #     browser.find_element(By.XPATH, '//div[@id="qc-cmp2-ui"]//button[@class=" css-6dzqka"]').click()
+            # except:
+            #     None
             browser.find_elements(By.XPATH, "//a[@class='lv-button-component new-button-style lv-dark-btn ng-star-inserted']")[0].click()
             time.sleep(0.5)
             browser.find_element(By.XPATH, "//h5[@class='h5-mobile-small display-web-short']").click()
@@ -66,6 +77,7 @@ if __name__ == "__main__":
             print('结束任务并退出')
             print('========================')
             time.sleep(3)
-        except:
+        except Exception as ex:
             print('*** 任务出错，重试 ***')
+            print(str(ex))
         browser.quit()
